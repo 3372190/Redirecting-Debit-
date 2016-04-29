02e29a8e5b01805de7e359c2f9c8bbbfa39ee9be
Gl3nda85 <s3249319@student.rmit.edu.au>
2016-04-29 21:44:54 +1000

    users can now see all service providers

--- inc/firebase/pageoverall.js
+++ inc/firebase/pageoverall.js
@@ -2,6 +2,7 @@ var firebaseRef = new Firebase("https://redirectdebit.firebaseio.com");
 var uId;
 $(document).ready(function(){
     loadUserServiceProviders();
+    loadProviders();
     
 });
 
@@ -9,19 +10,37 @@ $(document).ready(function(){
 function loadUserServiceProviders(){
     
     firebaseRef.child('users').child(uId).child("serviceproviders").once('value', function(userSnap) {
+        var numOfProviders = userSnap.numChildren();
+        $("#numofproviders").html(numOfProviders);
         userSnap.forEach(function(childSnapshot){
             firebaseRef.child('serviceprovider').child(childSnapshot.key()).once('value', function(mediaSnap) {
                 var serviceResults = mediaSnap.val();
                 var userResults = childSnapshot.val();
 
                 
-                 $('#serviceoverall > tbody:last-child').append('<tr><td><img class="rounded-x" src="'+serviceResults.img+'" alt=""></td><td class="td-width"><p>'+serviceResults.description+'</p></td><td><span class="label label-success">'+userResults.notified+'</span></td><td><span><a href="#">'+serviceResults.email+'</a></span><span><a href="#">'+serviceResults.website+'</a></span></td></tr>');
+                 $('#serviceoverall > tbody:last-child').append('<tr><td><img class="rounded-x" src="'+serviceResults.img+'" alt=""></td><td class="td-width"><p>'+serviceResults.description+'</p></td><td><span class="label label-success">'+userResults.notified+'</span></td><td><span><a href="#">'+serviceResults.email+'</a></span><br><span><a href="#">'+serviceResults.website+'</a></span></td></tr>');
             });
     
         });
 
     });
     
+    
+}
+
+function loadProviders(){
+    providersRef = firebaseRef.child("serviceprovider");
+    providersRef.limit(2).once('value', function(providerSnap){
+        providerSnap.forEach(function(childSnapshot){
+        var serviceResults = childSnapshot.val();
+        
+        $('#providerrow').append('<div class="col-sm-6"><div class="profile-blog blog-border"><img class="rounded-x" src="'+serviceResults.img+'" alt=""><div class="name-location"><strong>'+serviceResults.name+'</strong></div><div class="clearfix margin-bottom-20"></div><p>'+serviceResults.description+'</p><hr><ul class="list-inline share-list"><li><a href="'+serviceResults.email+'">website</a></li><li><i class="fa fa-facebook"></i><a href="#">54 Followers</a></li><li><i class="fa fa-twitter"></i><a href="#">Retweet</a></li></ul></div></div>');
+              
+        });
+        
+        
+        
+    });
     
 }
 
