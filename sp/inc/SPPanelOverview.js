b63719c73578819b52fd469ff0f889a8da2e6245
Gl3nda85 <s3249319@student.rmit.edu.au>
2016-06-03 11:17:01 +1000

    patched service Panel

--- sp/inc/SPPanelOverview.js
+++ sp/inc/SPPanelOverview.js
@@ -13,33 +13,38 @@ function loadUserServiceProviders(){
         // The callback function will get called twice, once for "fred" and once for "barney"
         snapshot.forEach(function(childSnapshot) {
             // key will be "fred" the first time and "barney" the second time
-            var key = childSnapshot.key();
+            var userKey = childSnapshot.key();
             // childData will be the actual contents of the child
             if(childSnapshot.child("serviceproviders").child(getUserId()).val() != null){
                 var childDataSP = childSnapshot.child("serviceproviders").child(getUserId()).val();
                 var childDataSPKey = childSnapshot.child("serviceproviders").child(getUserId()).key();
                 var childData = childSnapshot.val();
                 var method;
-                if(childDataSP.method == "callback"){
-                    method = "Callback requested " + childData.phonenumber;
-                }else{
-                    method = '<a href="google.com.au">Click Here</a>';
+
+                if (!childDataSP.responded) {
+
+                    if (childDataSP.method == "callback") {
+                        method = "Callback requested " + childData.phonenumber;
+                    } else {
+                        method = '<a href="google.com.au">Click Here</a>';
+                    }
+
+                    if (childDataSP.notified) {
+                        $('#serviceOverallTable > tbody:last-child')
+                            .append('<tr id="' + userKey + '"><td><img src="./../' + childData.profileimage + '" alt="./../assets/img/team/img32-md.jpg"/>' +
+                                ' <br><h4>' + childData.firstname + " " + childData.lastname + '</h4></td>' +
+                                '<td>' +
+                                '<br>' + childData.emailaddress +
+                                '<br>' + childData.address +
+                                '<br>' + childData.state + ", " + childData.postcode +
+                                '<br>' + childData.country +
+                                '</td><td>' + method + '</td>' +
+                                '<td style="text-align: center;"><input type="image" width="40" height="40" src="./../assets/img/tick_unselected.png" onclick="confirmComplete(\'' + childDataSPKey + '\',\'' + userKey + '\')" /></td></tr>');
+
+
+                    }
                 }
 
-                if(childDataSP.notified){
-                    $('#serviceOverallTable > tbody:last-child')
-                        .append('<tr id="' + childDataSPKey + '"><td><img src="./../' + childData.profileimage + '" alt="./../assets/img/team/img32-md.jpg"/>' +
-                            ' <br><h4>'+childData.firstname + " " + childData.lastname+ '</h4></td>' +
-                            '<td>' +
-                            '<br>' + childData.emailaddress +
-                            '<br>' + childData.address +
-                            '<br>' + childData.state + ", " + childData.postcode +
-                            '<br>' + childData.country +
-                            '</td><td>'+method+'</td>' +
-                            '<td style="text-align: center;"><input type="image" width="40" height="40" src="./../assets/img/tick_unselected.png" onclick="confirmComplete(\'' + childDataSPKey + '\',\'' + key+ '\')" /></td></tr>');
-
-
-                }
             }
         });
     });
@@ -50,7 +55,7 @@ function confirmComplete(spKey, uKey){
         var x;
         if (confirm("Are You Sure?") == true) {
             updateUser(spKey, uKey);
-            $('#'+spKey+'').remove();
+            $('#' + uKey + '').remove();
         } else {
             x = "You pressed Cancel!";
             console.log(x);
