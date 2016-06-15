d4f120c11ed90c6533e4a08998da738403bde34d
Sulien <S3436413@student.rmit.edu.au>
2016-06-15 18:13:42 +1000

    RDASPA finds more than 1, broken JSON.

--- inc/classes/rdaspa.php
+++ inc/classes/rdaspa.php
@@ -30,26 +30,31 @@ class rdaspa{
         {
             date_default_timezone_set('GMT');
         }
-		
-		for ($i = 0; $i < count($iList); $i++)		
+
+		for ($i = 0; $i < count($this->iList); $i++)		
 		{
 			$currDate = strtotime('1 month ago', strtotime($iList[$i]->getDate()));	//Reference date (1 month before original)
 			$startDate = strtotime('-3 days', $currDate);							//3 days prior to 1 month reference
 			$endDate = strtotime('+3 days', $currDate);								//3 days after 1 month reference
 
+			//Save some cycles.
 			if (strcmp($iList[$i]->getAmount(), " ") == 0)
 			{
-
-			} else {
-				for ($j = $i + 1; $j < count($iList); $j++) {
-
-					$newDate = strtotime($iList[$j]->getDate());                        //Date to check against reference
-
-					if ($newDate > $startDate && $newDate < $endDate)                    //1 month +- 3 days
+			} else {	//Starting with the entry beneath the current one. Check the dates.
+				for ($j = $i + 1; $j < count($this->iList); $j++) {
+					
+					//Date to check against reference
+					$newDate = strtotime($this->iList[$j]->getDate());                        
+					
+					//Check if new date is 1 month +- 3 days prior to current date.
+					if ($newDate > $startDate && $newDate < $endDate)                    
 					{
-						if ($this->checkTokens($j))            //If dates are a go, check if token in description exists in SP database.
+						
+						//Check if the potential SP entry has a keyword in RedirectDebit SP Database.
+						if ($this->checkTokens($j))
 						{
-							array_push($this->spList, $iList[$j]);
+							//Add it to the SP List.
+							array_push($this->spList, $this->iList[$j]);
 						}
 					}
 				}
@@ -70,14 +75,11 @@ class rdaspa{
 			$token = strtolower($token);
 			//Compare to names in providerList
 			$this->providerList[$n] = strtolower($this->providerList[$n]);
-
-			//Every token except first token was upper case -_-
 			
 			while ($token != NULL) 
 			{
 				if (strcmp($token, $this->providerList[$n]) == 0)                //If token in description matches SP database
 				{
-					//When there's a match. Check to see if this provider has already been found.
 					if ($this->checkExistence($token))                    //Check if we already found that provider.
 					{
 						$this->iList[$k]->setName($token);
@@ -94,20 +96,22 @@ class rdaspa{
 	}
 
 	function checkExistence($token)            //Check to see if $token already assigned to name in spList
-	{
-		$i;
+	{		
+		$g;
 		if (count($this->spList) == 0) {
 			return true;
 		} else {
-			for ($i = 0; $i < count($this->spList); $i++) {
-				if (strcmp($token, $this->spList[$i]->getName() == 0)) {
+			
+			for ($g = 0; $g < count($this->spList); $g++) {
+				if (strcmp($token, $this->spList[$g]->getName()) == 0) {
+					
 					return false;
-					//array_push($this->spList, $this->foundList[$i]);			//Add object to found list/
+					
 				}
 			}
 		}
 
-		return true;
+		return true; 
 
 	}
 	
