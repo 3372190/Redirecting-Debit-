b96bdbabe32a728e8543af8863363f113b7f89e6
Gl3nda85 <s3249319@student.rmit.edu.au>
2016-06-24 12:13:11 +1000

    FAQ

--- page_faq.php
+++ page_faq.php
@@ -56,7 +56,7 @@
 							</div>
 							<div id="collapseOne" class="panel-collapse collapse in">
 								<div class="panel-body">
-									Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.
+									Please submit a request for your bank and we will work on getting that bank setup
 								</div>
 							</div>
 						</div>
